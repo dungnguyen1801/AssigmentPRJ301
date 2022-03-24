8847a83e385fa60add70cea48a3091bf6168d6ea
dungnguyen1801 <95198070+dungnguyen1801@users.noreply.github.com>
2022-03-24 19:20:56 +0700

    User class

--- User.java
+++ User.java
@@ -3,123 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package model;
 
-import dal.UserDAO;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import java.util.logging.Logger;
 
 /**
- * ControllerServlet.java
- * This servlet acts as a page controller for the application, handling all
- * requests from the user.
- * @email Ramesh Fadatare
+ *
+ * @author DUNG
  */
+public class User {
+    private String id;
+    private String name;
 
-@WebServlet("/")
-public class User extends HttpServlet {
+    public User() {
+    }
 
     public User(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
-	private static final long serialVersionUID = 1L;
-	private UserDAO userDAO;
-	
-	public void init() {
-		userDAO = new UserDAO();
-	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
+    public String getId() {
+        return id;
+    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String action = request.getServletPath();
+    public void setId(String id) {
+        this.id = id;
+    }
 
-		try {
-			switch (action) {
-			case "/new":
-				showNewForm(request, response);
-				break;
-			case "/insert":
-				insertUser(request, response);
-				break;
-			case "/delete":
-				deleteUser(request, response);
-				break;
-			case "/edit":
-				showEditForm(request, response);
-				break;
-			case "/update":
-				updateUser(request, response);
-				break;
-			default:
-				listUser(request, response);
-				break;
-			}
-		} catch (SQLException ex) {
-			throw new ServletException(ex);
-		}
-	}
+    public String getName() {
+        return name;
+    }
 
-	private void listUser(HttpServletRequest request, HttpServletResponse response)
-			throws SQLException, IOException, ServletException {
-		List<model.User> listUser = userDAO.selectAllUsers();
-		request.setAttribute("listUser", listUser);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
-		dispatcher.forward(request, response);
-	}
-
-	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
-		dispatcher.forward(request, response);
-	}
-
-	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
-			throws SQLException, ServletException, IOException {
-		int id = Integer.parseInt(request.getParameter("id"));
-		model.User existingUser = userDAO.selectUser(id);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
-		request.setAttribute("user", existingUser);
-		dispatcher.forward(request, response);
-
-	}
-
-	private void insertUser(HttpServletRequest request, HttpServletResponse response) 
-			throws SQLException, IOException {
-		String id = request.getParameter("id");
-		String name = request.getParameter("name");
-		User newUser = new User(id,name);
-		userDAO.insertUser(newUser);
-		response.sendRedirect("list");
-	}
-
-	private void updateUser(HttpServletRequest request, HttpServletResponse response) 
-			throws SQLException, IOException {
-		String id = request.getParameter("id");
-		String name = request.getParameter("name");
-		User book = new User(id,name);
-		userDAO.updateUser(book);
-		response.sendRedirect("list");
-	}
-
-	private void deleteUser(HttpServletRequest request, HttpServletResponse response) 
-			throws SQLException, IOException {
-		int id = Integer.parseInt(request.getParameter("id"));
-		userDAO.deleteUser(id);
-		response.sendRedirect("list");
-
-	}
-
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+   
 }
